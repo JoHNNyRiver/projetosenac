5a0bf1804452555d64db4edbf35bf478c24822d6
johnnyriver <jlribeiroaugusto@gmail.com>
2018-12-01 10:54:10 -0200

    algumas coisas feitas

--- app/src/main/java/com/example/joao/facesenac/pi/activity/activity/PostImageActivity.java
+++ app/src/main/java/com/example/joao/facesenac/pi/activity/activity/PostImageActivity.java
@@ -1,21 +1,47 @@
 package com.example.joao.facesenac.pi.activity.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import com.example.joao.facesenac.R;
+import com.example.joao.facesenac.pi.activity.interfaces.ApiUsers;
+import com.example.joao.facesenac.pi.activity.model.PostFeed;
+import com.example.joao.facesenac.pi.activity.model.PostUserLogin;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class PostImageActivity extends AppCompatActivity {
-    private ImageView imageCamera;
     private static final int SELECTION_CODE = 200;
+    private ImageView imageCamera;
+    private EditText editTextImage;
+    private Button buttonPostarDaImagem;
+    private Long idGeneral;
+    private Bitmap scaledImage = null;
+    private ProgressBar progressBarPostTwo;
+    private ImageView imageViewCamera, imageViewDispositivo;
+    private Bitmap scaleImage300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +51,109 @@ public class PostImageActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
-        ImageView imageViewCamera = findViewById(R.id.imageViewCamera);
-        ImageView imageViewDispositivo = findViewById(R.id.imageViewDispositivo);
+        SQLiteDatabase sqlite = openOrCreateDatabase("app", MODE_PRIVATE, null);
+        PostUserLogin user = new PostUserLogin();
+        Cursor cursor = sqlite.rawQuery("SELECT * FROM user", null);
+
+        int id = cursor.getColumnIndex("id");
+        int nomeidx = cursor.getColumnIndex("nome");
+        int senhas = cursor.getColumnIndex("senha");
+        int email = cursor.getColumnIndex("email");
+        int fotoTwo = cursor.getColumnIndex("foto");
+
+        while (cursor.moveToFirst()) {
+            user.setId(cursor.getLong(id));
+            user.setNome(cursor.getString(nomeidx));
+            user.setSenha(cursor.getString(senhas));
+            user.setEmail(cursor.getString(email));
+            user.setTemfoto(cursor.getInt(fotoTwo));
+
+            break;
+        }
+
+        if (cursor.moveToFirst()) {
+            idGeneral = user.getId();
+
+            sqlite.close();
+            cursor.close();
+        }
+
+        editTextImage = findViewById(R.id.editTextImage);
+        buttonPostarDaImagem = findViewById(R.id.buttonPostarDaImagem);
         imageCamera = findViewById(R.id.imageCamera);
+        progressBarPostTwo = findViewById(R.id.progressBarPostTwo);
+
+        imageViewCamera = findViewById(R.id.imageViewCamera);
+        imageViewDispositivo = findViewById(R.id.imageViewDispositivo);
+
+        buttonPostarDaImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBarPostTwo.setVisibility(View.VISIBLE);
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                scaleImage300.compress(Bitmap.CompressFormat.JPEG, 65, byteArrayOutputStream);
+
+                disabledOrEnabled(false);
+
+                Retrofit postFeed = new Retrofit.Builder()
+                        .baseUrl("https://pi4facenac.azurewebsites.net/PI4/api/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                ApiUsers apiFeed = postFeed.create(ApiUsers.class);
+                String textoValue = editTextImage.getText().toString();
+                String fotoValue = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+
+                PostFeed postFeeder = new PostFeed();
+                postFeeder.setTexto(textoValue);
+                postFeeder.setId(idGeneral);
+                postFeeder.setFoto(fotoValue);
+
+                Call<Long> callFeed = apiFeed.postFeed(postFeeder);
+
+                Callback<Long> callback = new Callback<Long>() {
+                    @Override
+                    public void onResponse(Call<Long> call, Response<Long> response) {
+                        Long resposta = response.body();
+                        progressBarPostTwo.setVisibility(View.GONE);
+                        disabledOrEnabled(true);
+
+                        if (response.code() == 200) {
+                            if (response.isSuccessful() && resposta != null) {
+                                Toast.makeText(PostImageActivity.this,
+                                        "successfull",
+                                        Toast.LENGTH_LONG).show();
+
+                                finish();
+                            } else {
+                                Toast.makeText(PostImageActivity.this,
+                                        "Houve um erro!",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(PostImageActivity.this,
+                                    "Houve um erro!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Long> call, Throwable t) {
+                        t.printStackTrace();
+
+                        progressBarPostTwo.setVisibility(View.GONE);
+                        disabledOrEnabled(true);
+
+                        Toast.makeText(PostImageActivity.this,
+                                "Houve um erro!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                };
+
+                callFeed.enqueue(callback);
+            }
+        });
 
         imageViewCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,30 +180,64 @@ public class PostImageActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bitmap scaledImage = null;
         int REQUEST_IMAGE_CAPTURE = 51;
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             imageCamera.setVisibility(View.VISIBLE);
+            editTextImage.setVisibility(View.VISIBLE);
+            buttonPostarDaImagem.setVisibility(View.VISIBLE);
 
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            scaledImage = Bitmap.createScaledBitmap(imageBitmap, 500, 500, true);
+            Bitmap imageBitmapTwo = (Bitmap) extras.get("data");
+
+            int largura = imageBitmap.getWidth();
+            int altura = imageBitmap.getHeight();
+            int x = (150 * largura) / altura;
+
+            int larguraT = imageBitmapTwo.getWidth();
+            int alturaT = imageBitmapTwo.getHeight();
+            int n = (300 * larguraT) / alturaT;
+
+            scaledImage = Bitmap.createScaledBitmap(imageBitmap, x, 150, true);
+            scaleImage300 = Bitmap.createScaledBitmap(imageBitmapTwo, n, 300, true);
             imageCamera.setImageBitmap(scaledImage);
         }
 
         if (resultCode == RESULT_OK && requestCode == SELECTION_CODE) {
             imageCamera.setVisibility(View.VISIBLE);
+            editTextImage.setVisibility(View.VISIBLE);
+            buttonPostarDaImagem.setVisibility(View.VISIBLE);
+
             Uri localImagem = Objects.requireNonNull(data).getData();
             Bitmap imagem = null;
+            Bitmap imagemTwo = null;
 
             try {
                 imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagem);
-                scaledImage = Bitmap.createScaledBitmap(imagem, 500, 500, true);
+                imagemTwo = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagem);
+
+                int largura = imagem.getWidth();
+                int altura = imagem.getHeight();
+                int x = (150 * largura) / altura;
+
+                int larguraT = imagemTwo.getWidth();
+                int alturaT = imagemTwo.getHeight();
+                int n = (300 * larguraT) / alturaT;
+
+                scaledImage = Bitmap.createScaledBitmap(imagem, x, 150, true);
+                scaleImage300 = Bitmap.createScaledBitmap(imagemTwo, n, 300, true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             imageCamera.setImageBitmap(scaledImage);
         }
+    }
+
+    private void disabledOrEnabled(Boolean enabled) {
+        buttonPostarDaImagem.setEnabled(enabled);
+        imageViewDispositivo.setEnabled(enabled);
+        imageViewCamera.setEnabled(enabled);
     }
 }
